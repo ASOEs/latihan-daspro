1f9cd57aeb3977de26e361fd2389138f20a1420e
ASOE <soewardiachmad40@gmail.com>
2023-10-18 07:03:36 +0700

    first comit

---
+++ dictionary/dictionary.py
@@ -0,0 +1,15 @@
+# list -> aray, mengakses data dengan menggunakan index
+# contoh
+# data_list = [1,2,3,4,5]
+# print(data_list[0])
+
+# dictionary (dict)  --> associative array
+# identifier --> key untuk mengakses data 
+data_dict = {
+    'key': 'value',
+    'cp' : 'ucupp',
+    'tg' : 'otong',
+    'dg' : 'dudung'
+}
+
+print(data_dict['cp'])
