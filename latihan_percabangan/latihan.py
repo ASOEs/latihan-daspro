546a878f7310b47ededaa51e55421f043930e372
ASOE <soewardiachmad40@gmail.com>
2023-10-20 08:40:39 +0700

    lathina loop

--- latihan_percabangan/latihan.py
+++ latihan_percabangan/latihan.py
@@ -1,13 +1,22 @@
-umur = int(input("masukan umur anda : "))
+# umur = int(input("masukan umur anda : "))
 
-if umur == 0 :
-    print("tidak valid")
+# if umur == 0 :
+#     print("tidak valid")
 
-elif umur < 18 :
-    print("remaja")
+# elif umur < 18 :
+#     print("remaja")
 
-elif umur < 65:
-    print("dewasa")
+# elif umur < 65:
+#     print("dewasa")
 
-else :
-    print("anda sudah tua")
+# else :
+#     print("anda sudah tua")
+
+
+while True :
+    angka = int(input("masukan angka anda : "))
+    if angka > 50 and angka % 2 == 1 :
+        print("benar")
+        break
+    else:
+        print("masukan angka yang benar")
