999e3cead49a19d6dfb9abacae6b9e25b68fefd7
ASOE <soewardiachmad40@gmail.com>
2023-10-24 11:17:41 +0700

    lingkaran

---
+++ latihan_flowchrt luas lingkaran/lingkaran.py
@@ -0,0 +1,8 @@
+jari_jari = float(input("masukan jari jari : "))
+phi = 3.14
+
+# luas lingkaran adalah 
+# L = phi **r
+
+luas = phi * (jari_jari**2)
+print(f"ini adalah luas lingkarannya : {luas}")
